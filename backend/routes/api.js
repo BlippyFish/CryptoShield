2308409879fb7ed020ddd45314f1beec1d49139f
Yoshimi Araki <yoshimi@Yoshimis-Laptop.local>
2024-07-24 22:15:51 -0400

    database connected

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const tiApiController = require('../controllers/tiApiController');
+const SB_controller = require('../supabaseClient')
 const router = express.Router();
 
 // GET Requests to retrieve data using the middleware routes
+
+router.get('/test_get_data', SB_controller.get_data_test, (req, res) => {
+  return res.status(200);
+})
 
 router.get('/coins', tiApiController.coinListMiddleware, (req, res) => {
   return res.status(200).json(res.locals);
--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,8 +1,37 @@
-import { createClient } from '@supabase/supabase-js';
-
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 const supabaseUrl = 'https://ptdcusrimsowtumozeln.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const SBcontroller = {};
+
+// this is test function to check if the data is connected
+SBcontroller.get_data_test = async (req, res, next) => {
+
+    try {
+        console.log(supabase)
+
+        const { data, error }  = await supabase
+            .from('test')
+            .select('*')
+        
+        
+  
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        console.log('Data fetched successfully:', data);
+        res.json(data);
+      }
+    } catch (error) {
+      console.error('could not fetch data, not connected:', error);
+    }
+  
+  
+  }
+
+  module.exports = SBcontroller;
 
 
 
